3e2c7c63c4524d6c0c8841f6986ae7873822b02f
Bruno Arouca <brunoarouca1@gmail.com>
2023-04-05 15:46:41 -0300

    Acrescentando erro 404 - pagina nao encontrada

--- src/server.ts
+++ src/server.ts
@@ -7,5 +7,9 @@ const server = express();
 server.use('/',router);
 server.use('/painel', routerPainel);
 
+server.use((req: Request, res: Response) => {
+  res.status(404).send('Error 404 - Página não encontrada')
+})
+
 
 server.listen(3000, () => { console.log('🔥 server on http://localhost:3000')});
