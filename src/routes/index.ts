ac85f940517f672da371b5ba78634d49b123d758
Bruno Arouca <brunoarouca.dev@gmail.com>
2023-04-06 00:39:08 -0300

    Acrescentado parte do estudo sobre template engine

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,36 @@
 import { Router, Request, Response } from 'express';
 
 const router = Router();
+const menu = ['Inicio', 'Sobre', 'Raças', 'Cuidado', 'Contato']
 
 router.get('/', (req: Request, res: Response) => {
-  res.send('Pagina Home')
+
+  res.render('pages/home', {
+    titlePage: "Home",
+    name: 'Bruno',
+    lastname: 'Arouca',
+    menu,
+    racas:[
+      {nome: 'Labrador Retriever', porte: 'Grande'},
+      {nome: 'Golden Retriever', porte: 'Grande'},
+      {nome: 'Bulldog Francês', porte: 'Médio'},
+      {nome: 'Poodle', porte: 'Pequeno'}
+    ]
+  })
 });
 
 router.get('/contato', (req: Request, res: Response) => {
-  res.send('Formulário de contato')
+  res.render('pages/contato', {
+    titlePage: 'Contato',
+    menu
+  })
 });
 
 router.get('/sobre', (req: Request, res: Response) => {
-  res.send('Minha pagina Institucional sobre a empresa')
+  res.render('pages/sobre', {
+    titlePage: 'Sobre nós',
+    menu
+  })
 });
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
 import express, {Request, Response} from 'express';
-import router from './routes/index';
+import path from 'path';
+import mustache from 'mustache-express';
+
+import mainRouter from './routes/index';
 import routerPainel from './routes/painel';
 
 const server = express();
 
-server.use('/',router);
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
+
+server.use('/',mainRouter);
 server.use('/painel', routerPainel);
+server.use(express.static(path.join(__dirname, '../public')));
+
+
 
 server.use((req: Request, res: Response) => {
   res.status(404).send('Error 404 - Página não encontrada')
